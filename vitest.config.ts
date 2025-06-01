84c86624e160d82ebbd81204448555055b3bfd03
Jeroen van den Burg <jeroen.vdburg@hotmail.nl>
2025-06-01 20:34:40 +0200

    fix ci

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,23 @@
-import { defineConfig } from "vitest/config";
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
     coverage: {
       enabled: true,
+      provider: 'v8', // Ensure you're using the correct provider (e.g., 'v8' or 'c8')
+      reporter: ['json', 'text', 'lcov'], // Add 'json' to generate a JSON report
+      reportsDirectory: './coverage', // Specify the directory for coverage reports
+       thresholds:
+      process.env.CI_SKIP_COVERAGE_THRESHOLDS === 'true'
+          ? undefined
+          : {
+              autoUpdate: false,
+              branches: 80,
+              functions: 80,
+              lines: 80,
+              statements: 80,
+              // perFile: true,
+            },
     },
   },
 });

907ae19e6219807f29da0091bf22df1ff6846dc1
Jeroen van den Burg <jeroen.vdburg@hotmail.nl>
2025-06-01 20:45:46 +0200

    fix vitest config syntax

--- vitest.config.ts
+++ vitest.config.ts
@@ -7,17 +7,15 @@ export default defineConfig({
       provider: 'v8', // Ensure you're using the correct provider (e.g., 'v8' or 'c8')
       reporter: ['json', 'text', 'lcov'], // Add 'json' to generate a JSON report
       reportsDirectory: './coverage', // Specify the directory for coverage reports
-       thresholds:
-      process.env.CI_SKIP_COVERAGE_THRESHOLDS === 'true'
-          ? undefined
-          : {
-              autoUpdate: false,
-              branches: 80,
-              functions: 80,
-              lines: 80,
-              statements: 80,
-              // perFile: true,
-            },
+      thresholds: process.env.CI_SKIP_COVERAGE_THRESHOLDS === 'true'
+        ? undefined
+        : {
+            autoUpdate: false,
+            branches: 80,
+            functions: 80,
+            lines: 80,
+            statements: 80,
+          },
     },
   },
 });
